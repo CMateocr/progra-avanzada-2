ee642f1d0a58e85295f3de0832e0841525d8a4a9
Mateo <mateocuricho28@gmail.com>
2025-12-18 19:26:48 -0500

    service being used

--- 01-jpa-example/src/main/java/com/programacion/avanzada/MainJpa01.java
+++ 01-jpa-example/src/main/java/com/programacion/avanzada/MainJpa01.java
@@ -1,9 +1,11 @@
 package com.programacion.avanzada;
 
+import com.programacion.avanzada.application.ManualLibraryService;
 import com.programacion.avanzada.domain.entity.Author;
 import com.programacion.avanzada.domain.entity.Book;
 import com.programacion.avanzada.domain.repository.IAuthorRepository;
 import com.programacion.avanzada.domain.repository.IBookRepository;
+import com.programacion.avanzada.domain.repository.ICustomerRepository;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
@@ -12,27 +14,11 @@ import java.math.BigDecimal;
 public class MainJpa01 {
   public static void main(String[] args) {
     try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-      IAuthorRepository authorRepository = container.select(IAuthorRepository.class).get();
-      IBookRepository bookRepository = container.select(IBookRepository.class).get();
+      var ma = container.select( ManualLibraryService.class ).get();
 
-//      Author a1 = Author
-//              .builder()
-//              .name("Mateo 1")
-//              .version(1)
-//              .build();
-//
-//      authorRepository.save(a1);
-//
-//      Book b1 = Book.builder()
-//              .isbn("2")
-//              .price(BigDecimal.valueOf(19.99))
-//              .title("El manantial")
-//              .version(1)
-//              .build();
-//
-//      b1.getAuthors().add( a1 );
-
-//      bookRepository.save( b1 );
+//      ma.createBookManual("lol", "Juan");
+//      ma.createOrderManual(10L);
     }
+
   }
 }
